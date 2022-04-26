91dfd7df06003d1f06244a351e8eeb30cac9ad4e
Thorsten Kurth <tkurth@nvidia.com>
2022-04-26 06:34:21 -0700

    adding printf

--- csrc/multi_tensor_lamb_mp.cu
+++ csrc/multi_tensor_lamb_mp.cu
@@ -205,6 +205,8 @@ struct LAMBStage1Functor
             r_v[ii] = MATH_T(0);
           }
         }
+	if(i_start + threadIdx.x == 0) printf("grads %f %f %f %f\n", r_g[0], r_g[1], r_g[2], r_g[3]);
+	
 #pragma unroll
         for(int ii = 0; ii < ILP; ii++)
         {
@@ -405,7 +407,9 @@ void multi_tensor_lamb_mp_cuda(
   assert(n_tensors == 4 || n_tensors == 5);
   using namespace at;
 
-  printf("n_tensors %i", n_tensors);
+  // debug
+  // printf("n_tensors %i\n", n_tensors);
+  // debug
   
   bool use_nvlamb = use_nvlamb_python.has_value() ? use_nvlamb_python.value() : false;
 
